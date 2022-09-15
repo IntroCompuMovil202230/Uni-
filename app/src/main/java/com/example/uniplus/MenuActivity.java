fa717334f0b369adbe95809b661eaf0d3dab471c
Juan David Romero <davidbouken@hotmail.com>
2022-09-15 11:47:56 -0500

    Se añadio la acatividad del chat de ayuda

--- app/src/main/java/com/example/uniplus/MenuActivity.java
+++ app/src/main/java/com/example/uniplus/MenuActivity.java
@@ -2,27 +2,37 @@ package com.example.uniplus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 
+import com.example.uniplus.databinding.ActivityMainBinding;
+import com.example.uniplus.databinding.ActivityMenuBinding;
 import com.google.android.material.imageview.ShapeableImageView;
 
 public class MenuActivity extends AppCompatActivity {
-ShapeableImageView vista;
+    ShapeableImageView vista;
+    ActivityMenuBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        binding = ActivityMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        vista = findViewById(R.id.cra13);
-
-        vista.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
+        binding.bottomNavegation.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.secondFragment:
+                    Intent intent = new Intent(getBaseContext(), ChatAyudaActivity.class);
+                    startActivity(intent);
+                    return true;
+                case R.id.thirdFragment:
+                    System.out.println("pasa al tercero");
+                    return true;
+                case R.id.fourthFragment:
+                    System.out.println("pasa al cuarto");
+                    return true;
             }
+            return true;
         });
     }
 }
