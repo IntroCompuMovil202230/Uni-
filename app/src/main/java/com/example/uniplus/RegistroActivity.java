190f2ec72eb70368ac9d2211f8a8d593fdf91130
txusdavid27 <jetraslavina@javeriana.edu.co>
2022-09-15 17:52:48 -0500

    CorrectReserva

--- app/src/main/java/com/example/uniplus/RegistroActivity.java
+++ app/src/main/java/com/example/uniplus/RegistroActivity.java
@@ -3,12 +3,23 @@ package com.example.uniplus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.uniplus.databinding.ActivityRegistroBinding;
 
 public class RegistroActivity extends AppCompatActivity {
+
+    private ActivityRegistroBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registro);
+        binding = ActivityRegistroBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        //binding.userNameField.setText();
+
+
     }
 }
