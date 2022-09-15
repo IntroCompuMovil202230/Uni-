747fe0e0bfb22ff05a18f487889313b877a10071
aospinaa <am.ospina@javeriana.edu.co>
2022-09-15 16:08:29 -0500

    Commits de David

---
+++ app/src/main/java/com/example/uniplus/MenuClienteActivity.java
@@ -0,0 +1,14 @@
+package com.example.uniplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuClienteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_cliente);
+    }
+}
