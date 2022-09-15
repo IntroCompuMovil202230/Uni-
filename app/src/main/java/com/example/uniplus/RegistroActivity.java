6dd1d65d94b0bab8e17126c3a7b47339efa7c6c3
txusdavid27 <jetraslavina@javeriana.edu.co>
2022-09-15 18:18:04 -0500

    Navega

--- app/src/main/java/com/example/uniplus/RegistroActivity.java
+++ app/src/main/java/com/example/uniplus/RegistroActivity.java
@@ -2,6 +2,7 @@ package com.example.uniplus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class RegistroActivity extends AppCompatActivity {
@@ -9,6 +10,34 @@ public class RegistroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+<<<<<<< Updated upstream
         setContentView(R.layout.activity_registro);
+=======
+        binding = ActivityRegistroBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.botonRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentSignIn = new Intent(getBaseContext(), MenuActivity.class);
+                startActivity(intentSignIn);
+            }
+        });
+
+        binding.addUserCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentSignIn = new Intent(getBaseContext(), TomarfotoActivity.class);
+                startActivity(intentSignIn);
+            }
+        });
+
+
+
+        //binding.userNameField.setText();
+
+
+>>>>>>> Stashed changes
     }
 }
--- app/src/main/java/com/example/uniplus/ReservaActivity.java
+++ app/src/main/java/com/example/uniplus/ReservaActivity.java
@@ -2,13 +2,43 @@ package com.example.uniplus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.uniplus.databinding.ActivityRegistroBinding;
+import com.example.uniplus.databinding.ActivityReservaBinding;
 
 public class ReservaActivity extends AppCompatActivity {
+
+    private ActivityReservaBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reserva);
+        //setContentView(R.layout.activity_reserva);
+        binding = ActivityReservaBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        
+        binding.bottomNavegation.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.secondFragment:
+                    Intent intentMapa = new Intent(getBaseContext(), MapaActivity.class);
+                    startActivity(intentMapa);
+                    return true;
+                case R.id.thirdFragment:
+                    Intent intentLista = new Intent(getBaseContext(), ListaActivity.class);
+                    startActivity(intentLista);
+                    return true;
+                case R.id.fourthFragment:
+                    Intent intentPerfil = new Intent(getBaseContext(), MenuClienteActivity.class);
+                    startActivity(intentPerfil);
+                    return true;
+            }
+            return true;
+        });
+
+
     }
 }
