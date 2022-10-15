055f4804291b7941143bebf05345678c5edb7286
txusdavid27 <jetraslavina@javeriana.edu.co>
2022-10-15 17:20:55 -0500

    Commit Navigate

--- app/src/main/java/com/example/uniplus/IniciarSesionActivity.java
+++ app/src/main/java/com/example/uniplus/IniciarSesionActivity.java
@@ -2,13 +2,33 @@ package com.example.uniplus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.uniplus.databinding.ActivityIniciarSesionBinding;
+import com.example.uniplus.databinding.ActivityRegistroBinding;
 
 public class IniciarSesionActivity extends AppCompatActivity {
+
+    ActivityIniciarSesionBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_iniciar_sesion);
+
+        binding = ActivityIniciarSesionBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+        setContentView(view);
+
+        binding.botonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentSignIn = new Intent(getBaseContext(), MenuActivity.class);
+                startActivity(intentSignIn);
+            }
+        });
+
     }
 }
